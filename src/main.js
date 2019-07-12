692407ed85efc378077e49e230268ba2d312bbc1
Answer1013 <841287025@qq.com>
2019-07-12 22:06:53 +0800

    更新gd模块路径

--- src/main.js
+++ src/main.js
@@ -9,6 +9,7 @@ Vue.prototype.$axios = axios
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import { Indicator } from 'mint-ui';
+import './module/gd.js'
 Vue.use(MintUI);
 axios.interceptors.request.use((config)=>{
   Indicator.open("正在骑马赶来的路上");
